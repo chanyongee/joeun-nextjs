779deb39fbda8f8a66088f9a4931aa8799ffd327
Chanyeong Kim <cykim@neosapience.com>
2024-11-20 23:51:34 +0900

    feat: navigation (PC)

---
+++ src/app/about/map/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutMapPage() {
+  return (
+    <div>
+      <h1>About Map Page</h1>
+    </div>
+  )
+}
--- src/style/fonts.ts
+++ src/style/fonts.ts
@@ -2,6 +2,5 @@ import { Noto_Sans_KR } from 'next/font/google'
 
 export const NotoSansKr = Noto_Sans_KR({
   subsets: ['latin'],
-  style: ['normal'],
-  weight: ['400', '500', '700'],
+  weight: ['300', '400', '500', '700'],
 })
